4af71721b0f2a3998ae43a64b059b27328259714
degesz <vojta.g@centrum.cz>
2020-08-19 13:41:27 +0200

    change port

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ app.get('/',function(req, res) {
 });
 app.use('/client',express.static(__dirname + '/client'));
  
+const port = process.env.PORT || 3000;
+
 serv.listen(2000);
 console.log("Server started.");
  

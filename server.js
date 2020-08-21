6df0b8be9ba1d0cb105dcf4c84c5a7ea4ab153e1
degesz <vojta.g@centrum.cz>
2020-08-21 11:57:54 +0200

    piskvorky

--- server.js
+++ server.js
@@ -13,40 +13,128 @@ serv.listen(2000);
 console.log("Server started.");
  
 var SOCKET_LIST = {};
+var GAME_LIST = [];
+var GAME_DATA = [];
+
+function gameDataObject(){
+	this.id = GAME_DATA.length;
+	this.players = [];
+	this.sockets = [];
+
+}
+
+function gameObject(){
+	this.founder;
+	this.id = GAME_LIST.length;
+	this.players = [];
+}
  
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
 	socket.id = Math.random();
-	socket.x = 0;
-	socket.y = 0;
-	socket.number = "" + Math.floor(10 * Math.random());
+	socket.joined = false;
+	//socket.name;
+	//socket.x = 0;
+	//socket.y = 0;
+	//socket.number = "" + Math.floor(10 * Math.random());
 	SOCKET_LIST[socket.id] = socket;
+
+	socket.on('setName', function(data){	//nastaveni jmena
+		socket.name = data.name;
+		console.log(socket.name);
+		sendGameList();
+	});
  
+	socket.on('newGame',function(){ 
+		var i;
+		for(i = 0; i < GAME_LIST.length; i++ ){
+			if (GAME_LIST[i].founder == socket.name) {
+				return;
+			}
+		}
+
+		if(socket.name == null){
+			return;
+		}
+		if(socket.joined){
+			return;
+		}
+
+		GAME_LIST.push(new gameObject);
+		GAME_LIST[GAME_LIST.length - 1].founder = socket.name;
+
+		GAME_DATA.push(new gameDataObject);
+		
+    	console.log(GAME_LIST);
+			sendGameList();
+	});
+
+	socket.on('join',function(data){
+		if(socket.joined){
+			console.log("already joined");
+			return;
+		}
+
+		id = data;
+
+		if(GAME_LIST[id].players.length >= 2){
+			console.log("game full");
+			return; 
+		}
+
+		socket.joined = true;		
+		GAME_LIST[id].players.push(socket.name);
+		GAME_DATA[id].players.push(socket.name);
+		GAME_DATA[id].sockets.push(socket);
+		sendGameList();
+
+		if(GAME_LIST[id].players.length >= 2){
+			startGame(id);
+		}
+	});
+
+	socket.on('myNewSquare',function(data){
+
+		console.log("new square from game id:" + data.id);
+
+		if (data.mySign == "x") {
+			GAME_DATA[data.gameId].sockets[1].emit('newCompetitorSquare', data);
+		}
+		if (data.mySign == "o") {
+			GAME_DATA[data.gameId].sockets[0].emit('newCompetitorSquare', data);
+		}
+		console.log("new square from game id:" + data.gameId);
+
+	});
+	
+
 	socket.on('disconnect',function(){
 		delete SOCKET_LIST[socket.id];
 	});
  
 });
- 
+
+function sendGameList(){
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('gameList',GAME_LIST);
+	}
+}
+
+function startGame(id){
+
+	console.log(GAME_DATA[id].sockets.length);
+
+	GAME_DATA[id].sockets[0].emit('startGame', 'x');
+	GAME_DATA[id].sockets[1].emit('startGame', 'o');
+}
+
+
+ /*
 setInterval(function(){
-	var pack = [];
-	for(var i in SOCKET_LIST){
-		var socket = SOCKET_LIST[i];
-		socket.x++;
-		socket.y++;
-		pack.push({
-			x:socket.x,
-			y:socket.y,
-			number:socket.number
-		});		
-	}
-	for(var i in SOCKET_LIST){
-		var socket = SOCKET_LIST[i];
-		socket.emit('newPositions',pack);
-	}
+
  
  
  
- 
-},1000/25);
- 
+},1000/25);		
+ */
